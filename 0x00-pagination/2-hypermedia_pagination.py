324df3d3689f67bdf9b7b423d166bb26daabf094
harkinder-dark <victorvaddely@gmail.com>
2023-05-12 13:26:35 +0000

    dadd first directory name pagination to pagination programming

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -68,7 +68,7 @@ class Server:
             "page_size": page_size,
             "page": page,
             "data": data,
-            "next_page": page + 1 if page < total_pages else None,
+            "next_page": page + 1 if page + 1 < total_pages else None,
             "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages
         }
