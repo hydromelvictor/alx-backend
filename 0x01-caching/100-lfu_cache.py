8d97a3a4902cb66453990d70e4d0232eb534481c
harkinder-dark <victorvaddely@gmail.com>
2023-05-17 05:12:39 +0000

    task 5

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -23,19 +23,23 @@ class LFUCache(BaseCaching):
             size = len(self.cache_data)
             if size >= BaseCaching.MAX_ITEMS:
                 if key not in self.cache_data:
-                    res_dict = dict(sorted(self.old.items(), key=lambda t: t[1][2]))
+                    res_dict = dict(sorted(self.old.items(),
+                                    key=lambda t: t[1][2]))
                     res = sorted(res_dict.items(), key=lambda t: t[1][2])[0]
                     del self.cache_data[res[0]]
                     del self.old[res[0]]
                     print("DISCARD: {}".format(res[0]))
             self.cache_data[key] = item
-            self.old[key] = (item, datetime.now(), 0 if key not in self.old.keys() else self.old[key][2] + 1)
+            self.old[key] = (item, datetime.now(),
+                             0 if key not in self.old.keys()
+                             else self.old[key][2] + 1)
 
     def get(self, key):
         """
         key : element key
         """
         if key in self.cache_data.keys():
-            self.old[key] = (self.old[key], datetime.now(), self.old[key][2] + 1)
+            self.old[key] = (self.old[key], datetime.now(),
+                             self.old[key][2] + 1)
             return self.cache_data.get(key)
         return None
