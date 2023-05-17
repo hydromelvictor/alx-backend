7a0ec6fe9a53902878616acadae8c65f7781ec6c
harkinder-dark <victorvaddely@gmail.com>
2023-05-17 05:09:17 +0000

    task 3

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -3,6 +3,7 @@
 Create a class BasicCache that inherits from
 BaseCaching and is a caching system:
 """
+from datetime import datetime
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -11,27 +12,30 @@ class LFUCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-    
+        self.old = {}
+
     def put(self, key, item):
         """
         key : element key
         item : element
         """
-        old = self.cache_data.copy()
         if key and item:
-            if len(self.cache_data) + 1 == BaseCaching.MAX_ITEMS:
-                for k, v in self.cache_data.items():
-                    if k in old and v != old[k]:
-                        del old[k]
+            size = len(self.cache_data)
+            if size >= BaseCaching.MAX_ITEMS:
                 if key not in self.cache_data:
-                    res = sorted(old.keys())[0]
-                    del self.cache_data[res]
-                    print("DISCARD: {}".format(res))
-    
+                    res_dict = dict(sorted(self.old.items(), key=lambda t: t[1][2]))
+                    res = sorted(res_dict.items(), key=lambda t: t[1][2])[0]
+                    del self.cache_data[res[0]]
+                    del self.old[res[0]]
+                    print("DISCARD: {}".format(res[0]))
+            self.cache_data[key] = item
+            self.old[key] = (item, datetime.now(), 0 if key not in self.old.keys() else self.old[key][2] + 1)
+
     def get(self, key):
         """
         key : element key
         """
         if key in self.cache_data.keys():
+            self.old[key] = (self.old[key], datetime.now(), self.old[key][2] + 1)
             return self.cache_data.get(key)
         return None
