40f99908b8f0fd216cb8c98df8cf7d784ea75b21
harkinder-dark <victorvaddely@gmail.com>
2023-05-17 04:41:57 +0000

    task 3

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -3,6 +3,7 @@
 Create a class MRUCache that inherits from
 BaseCaching and is a caching system:
 """
+from datetime import datetime
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -12,31 +13,29 @@ class MRUCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
+        self.old = {}
     
     def put(self, key, item):
         """
         key : element key
         item : element
         """
-        old = self.cache_data.copy()
-        new = {}
         if key and item:
             size = len(self.cache_data)
             if size >= BaseCaching.MAX_ITEMS:
-                new = {
-                    k: v for k, v in self.cache_data.items()
-                    if k in old.keys() and v != old[k]
-                    }
                 if key not in self.cache_data:
-                    res = sorted(new.keys() if len(new) != 0 else old.keys())[0]
-                    del self.cache_data[res]
-                    print("DISCARD: {}".format(res))
+                    res = sorted(self.old.items(), key=lambda t: t[1][1])[size - 1]
+                    del self.cache_data[res[0]]
+                    del self.old[res[0]]
+                    print("DISCARD: {}".format(res[0]))
             self.cache_data[key] = item
+            self.old[key] = (item, datetime.now())
     
     def get(self, key):
         """
         key : element key
         """
         if key in self.cache_data.keys():
+            self.old[key] = (self.old[key], datetime.now())
             return self.cache_data.get(key)
         return None
