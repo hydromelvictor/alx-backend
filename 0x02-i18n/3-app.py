7932e8d2f1fa99033c798448e2f1bcb6bf2a28e9
harkinder-dark <victorvaddely@gmail.com>
2023-05-23 14:09:09 +0000

    add new directory in alx-backend

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 """First you will setup a basic Flask app in 0-app.py
 """
 from flask import Flask, render_template, request, localeselector
-from flask_babel import Babel, Locale, timezone, _
+from flask_babel import Babel, Locale, timezone
 from typing import Optional, Text
 
 app = Flask(__name__)
@@ -14,22 +14,20 @@ class Config:
     configuration class
     """
     LANGUAGES = ["en", "fr"]
-
-
-babel.default_locale = Config.LANGUAGES[0]
-babel.default_timezone = timezone('UTC')
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 @babel.localeselector
 def get_locale():
     """get_locale"""
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(Config.BABEL_DEFAULT_LOCALE)
 
 
 @app.route("/", strict_slashes=False)
 def run() -> Text:
     """return templates/3-index.html"""
-    return render_template("3-index.html", lang=babel.default_locale)
+    return render_template("3-index.html", lang=Config.BABEL_DEFAULT_LOCALE)
 
 
 if __name__ == "__main__":
